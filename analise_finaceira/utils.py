326cf95a4198cfbbc2d494eb70824a68a8948d38
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-05-08 02:33:27 -0300

    Create utils.py

---
+++ analise_finaceira/utils.py
@@ -0,0 +1,21 @@
+import sqlite3
+import os
+from flask import current_app
+
+def get_db_connection():
+    db_path = current_app.config['DATABASE']
+    conn = sqlite3.connect(db_path)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_dashboard_highlights():
+    conn = get_db_connection()
+    try:
+        total_transacoes = conn.execute('SELECT COUNT(*) FROM transacoes').fetchone()[0]
+        total_alertas_ativos = conn.execute('SELECT COUNT(*) FROM alertas_financas WHERE ativo = 1').fetchone()[0]
+        total_categorias = conn.execute('SELECT COUNT(DISTINCT categoria) FROM transacoes').fetchone()[0]
+    except Exception:
+        total_transacoes = total_alertas_ativos = total_categorias = 0
+    finally:
+        conn.close()
+    return total_transacoes, total_alertas_ativos, total_categorias

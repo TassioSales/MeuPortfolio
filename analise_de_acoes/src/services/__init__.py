06284f9ee3582e843f30db29be69cd9629bee257
TassioSales <74218122+TassioSales@users.noreply.github.com>
2025-05-30 00:39:12 -0300

    Update __init__.py

--- analise_de_acoes/src/services/__init__.py
+++ analise_de_acoes/src/services/__init__.py
@@ -6,5 +6,17 @@ from .brapi_service import BrapiService
 from .yfinance_service import YFinanceService
 from .analise_pred import AnalisePred
 from .relatorios import Relatorios
+from .alerta_service import AlertaService
+from .ativo_service import AtivoService
+from .carteira_service import CarteiraService
 
-__all__ = ['BinanceService', 'BrapiService', 'YFinanceService', 'AnalisePred', 'Relatorios']
+__all__ = [
+    'BinanceService', 
+    'BrapiService', 
+    'YFinanceService', 
+    'AnalisePred', 
+    'Relatorios',
+    'AlertaService',
+    'AtivoService',
+    'CarteiraService'
+]

29d16906dc7d935a2c8d1ec39c79a8ff8e60d4f3
TassioSales <74218122+TassioSales@users.noreply.github.com>
2026-05-06 14:38:07 -0300

    Update start_documind.bat

--- documind_local/start_documind.bat
+++ documind_local/start_documind.bat
@@ -6,6 +6,6 @@ cd /d "%~dp0"
 echo Starting DocuMind Local at http://localhost:8093
 start "DocuMind Local" cmd /k "cd /d ""%~dp0backend"" && go run ./cmd/server"
 timeout /t 2 /nobreak >nul
-start "" "http://localhost:8093/?v=20260502-upload-fix"
+start "" "http://localhost:8093/?v=20260502-study-tabs"
 
 endlocal

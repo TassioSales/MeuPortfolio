dac95fd8d7139764be497a5d75578f8f6807b546
TassioSales <74218122+TassioSales@users.noreply.github.com>
2026-06-24 12:46:01 -0300

    Update urls.py

--- aprovacao_compras/core/urls.py
+++ aprovacao_compras/core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("cadastro/", views.registro_view, name="registro"),
     path("admin-dashboard/", views.admin_dashboard, name="admin_dashboard"),
     path("usuarios/", views.gerenciar_usuarios, name="gerenciar_usuarios"),
+    path("configuracoes/categorias/", views.gerenciar_categorias, name="gerenciar_categorias"),
 ]
